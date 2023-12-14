c72d6b3e164c6e10b04377787a6c72e87012cb49
Muhd Azfar Ramly <136482745+azfar29@users.noreply.github.com>
2023-12-14 11:55:54 +0800

    fixed emoji picker

--- pages/api/socket/io.ts
+++ pages/api/socket/io.ts
@@ -10,8 +10,8 @@ export const config = {
   },
 };
 
-const ioHandler = (res: NextApiResponseServerIo) => {
-  if (!res.socket?.server?.io) {
+const ioHandler = (req: NextApiRequest, res: NextApiResponseServerIo) => {
+  if (!res.socket.server.io) {
     const path = "/api/socket/io";
     const httpServer: NetServer = res.socket.server as any;
     const io = new ServerIO(httpServer, {
